4eca4ed6f142b7a0d3431f48ff0af3666ce9ba8e
Jenny Beate Hougen <Jenny.Beate.Hougen@fhi.no>
2025-05-08 14:58:22 +0200

    Add more tests

--- tests/Fhi.Authorization.Extensions.UnitTests/ScopeHandlerTests.cs
+++ tests/Fhi.Authorization.Extensions.UnitTests/ScopeHandlerTests.cs
@@ -5,9 +5,8 @@ namespace Fhi.Authorization.IntegrationTests
 {
     public class ScopeHandlerTests
     {
-
         [Test]
-        public void HandleRequirementAsync_ScopeExist_Ok()
+        public async Task HandleRequirementAsync_ScopeExist_Ok()
         {
             var scopeHandler = new ScopeHandler();
             var context = new AuthorizationHandlerContext(
@@ -17,10 +16,49 @@ namespace Fhi.Authorization.IntegrationTests
                     new Claim("scope", "test")
                 ])),
                 null);
-            scopeHandler.HandleAsync(context);
 
-            Assert.Pass();
+
+            await scopeHandler.HandleAsync(context);
+
+            Assert.That(context.HasSucceeded, Is.True);
+            Assert.That(context.HasFailed, Is.False);
         }
 
+        [Test]
+        public async Task HandleRequirementAsync_NoScopeClaim_Fail()
+        {
+            var scopeHandler = new ScopeHandler();
+            var context = new AuthorizationHandlerContext(
+                [new ScopeAttribute("test")],
+                new ClaimsPrincipal(new ClaimsIdentity(
+                [
+                ])),
+                null);
+
+
+            await scopeHandler.HandleAsync(context);
+
+            Assert.That(context.HasSucceeded, Is.False);
+            Assert.That(context.HasFailed, Is.True);
+        }
+
+        [Test]
+        public async Task HandleRequirementAsync_ScopeDoesNotExist_Fail()
+        {
+            var scopeHandler = new ScopeHandler();
+            var context = new AuthorizationHandlerContext(
+                [new ScopeAttribute("fail")],
+                new ClaimsPrincipal(new ClaimsIdentity(
+                [
+                    new Claim("scope", "test")
+                ])),
+                null);
+
+
+            await scopeHandler.HandleAsync(context);
+
+            Assert.That(context.HasSucceeded, Is.False);
+            Assert.That(context.HasFailed, Is.True);
+        }
     }
 }
