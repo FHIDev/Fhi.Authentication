81b2c24517484d5a963269ed3cef7666252863b7
jennyhougen <jenny.beate.hougen@fhi.no>
2025-05-16 19:13:27 +0200

    Update package version of Fhi.Authentication.Extensions (#2)
    * Update package version of Fhi.Authentication.Extensions and cleanup documentateion and Blazor sample

--- samples/Fhi.Samples.BlazorInteractiveServer/Hosting/Authentication/InMemoryTokenStore.cs
+++ samples/Fhi.Samples.BlazorInteractiveServer/Hosting/Authentication/InMemoryTokenStore.cs
@@ -1,22 +1,17 @@
 ﻿using Duende.AccessTokenManagement.OpenIdConnect;
 using Duende.IdentityModel;
-using Microsoft.AspNetCore.Authentication;
 using System.Collections.Concurrent;
 using System.Security.Claims;
 
 /// <summary>
 ///Blazor Server keeps a persistent SignalR connection. That connection does not have access to HttpContext or cookies directly.
-/// https://learn.microsoft.com/en-us/aspnet/core/blazor/state-management?view=aspnetcore-9.0&pivots=server
+/// https://learn.microsoft.com/en-us/aspnet/core/blazor/state-management?view=aspnetcore-9.0&pivots=server.
+/// This stores tokens in memory. For applications that use multiple instances or do not use sticky sessions, user state should be stored in persistent storage
+/// see https://learn.microsoft.com/en-us/aspnet/core/blazor/state-management?view=aspnetcore-9.0&pivots=server#server-side-storage-server
 /// </summary>
 public class InMemoryUserTokenStore : IUserTokenStore
 {
-    public InMemoryUserTokenStore(IHttpContextAccessor contextAccessor)
-    {
-        _contextAccessor = contextAccessor;
-    }
-
     private static readonly ConcurrentDictionary<string, UserToken> _tokenStore = new();
-    private readonly IHttpContextAccessor _contextAccessor;
 
     public Task StoreTokenAsync(ClaimsPrincipal user, UserToken token, UserTokenRequestParameters? parameters = null)
     {
@@ -30,7 +25,6 @@ public class InMemoryUserTokenStore : IUserTokenStore
 
     public Task<UserToken> GetTokenAsync(ClaimsPrincipal user, UserTokenRequestParameters? parameters = null)
     {
-        var access_token = _contextAccessor.HttpContext?.GetTokenAsync("access_token");
         var userId = user.FindFirst(JwtClaimTypes.Subject)?.Value;
         if (userId != null && _tokenStore.TryGetValue(userId, out var token))
         {
